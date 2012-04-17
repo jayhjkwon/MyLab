97fdd9d518483c8d9c18da3d9bb76dd630b51dc5
Ray H.J Kwon <nicekhj@gmail.com>
2012-04-17 18:08:45 +0900

    added moq test project

---
+++ MoqTest/Foo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MoqTest
+{
+    public interface IService
+    {
+        bool DoSomething(string param);
+    }
+
+    public class Service : IService
+    {
+
+        public bool DoSomething(string param)
+        {
+            if (param.Length % 2 == 0)
+                return true;
+            else
+                return false;
+        }
+    }
+
+    public class Controller
+    {
+        public virtual int Id { get; set; }
+        private IService service;
+
+        public Controller()
+        {
+        }
+
+        public Controller(IService service)
+        {
+            this.service = service;
+        }
+
+        public bool GetReult(string param)
+        {
+            return service.DoSomething(param);
+        }
+
+    }
+}
---
+++ MoqTest/UnitTest1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace MoqTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var mock = new Mock<IService>();
+            mock.Setup(x => x.DoSomething("test")).Returns(false);
+
+            IService service = mock.Object;
+            Controller client = new Controller(service);
+
+            bool result = client.GetReult("test");
+            
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var mock = new Mock<IService>();
+            mock.Setup(x => x.DoSomething(It.IsAny<string>())).Returns(false);
+
+            IService service = mock.Object;
+            Controller client = new Controller(service);
+
+            bool result = client.GetReult("hello");
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var mock = new Mock<IService>();
+            mock.Setup(x => x.DoSomething(It.Is<string>(s=>s.Length == 5))).Returns(false);
+
+            IService service = mock.Object;
+            Controller client = new Controller(service);
+
+            bool result = client.GetReult("hello");
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            var mock = new Mock<IService>();
+
+            bool check = true;
+            mock.Setup(x => x.DoSomething("test"))
+                .Returns(() => false)
+                .Callback(()=> check = false);
+
+            IService service = mock.Object;
+            Controller client = new Controller(service);
+
+            bool result = client.GetReult("test");
+
+            Assert.IsFalse(result);
+            Assert.IsFalse(check);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            var mock = new Mock<Controller>();
+
+            mock.Setup(x => x.Id).Returns(100);
+
+            Assert.AreEqual(100, mock.Object.Id);
+        }
+    }
+}
