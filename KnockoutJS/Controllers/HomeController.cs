29bbc6ee9ac14dcd95f2980d2f60e707b0f6f734
Ray H.J Kwon <nicekhj@gmail.com>
2012-05-10 19:27:08 +0900

    added knockoutjs test

--- KnockoutJS/Controllers/HomeController.cs
+++ KnockoutJS/Controllers/HomeController.cs
@@ -19,5 +19,54 @@ namespace KnockoutJS.Controllers
         {
             return View();
         }
+
+        public ActionResult First()
+        {
+            return View();
+        }
+
+        public ActionResult Second()
+        {
+            return View();
+        }
+
+        public ActionResult Third()
+        {
+            return View();
+        }
+
+        public ActionResult Fourth()
+        {
+            return View();
+        }
+
+        
+        public JsonResult GetPeopleViaAjax()
+        {
+            var people = new List<Person>
+                             {
+                                 new Person {Name = "권효중", Address = "Songdo"},
+                                 new Person {Name = "이수진", Address = "Seoul"},
+                                 new Person {Name = "박지성", Address = "London"}
+                             };
+
+            return Json(people, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult Create(List<Person> people)
+        {
+            var message = people.Aggregate("결과값\r\n", 
+                (current, person) => current + string.Format("{0} {1} {2}", person.Name, person.Address, Environment.NewLine));
+            
+            return Json(message);
+        }
+
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
     }
 }
