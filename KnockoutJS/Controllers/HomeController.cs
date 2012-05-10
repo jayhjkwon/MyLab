44eafa3fcd3cba5c56e53a03cba0633343772fbd
Ray H.J Kwon <nicekhj@gmail.com>
2012-05-10 19:53:36 +0900

    added cache:false

--- KnockoutJS/Controllers/HomeController.cs
+++ KnockoutJS/Controllers/HomeController.cs
@@ -8,6 +8,18 @@ namespace KnockoutJS.Controllers
 {
     public class HomeController : Controller
     {
+        private static List<Person> _people;
+ 
+        public HomeController()
+        {
+            
+            _people = _people ?? new List<Person>
+                             {
+                                 new Person {Name = "권효중", Address = "Songdo"},
+                                 new Person {Name = "이수진", Address = "Seoul"},
+                                 new Person {Name = "박지성", Address = "London"}
+                             };
+        }
         public ActionResult Index()
         {
             ViewBag.Message = "Welcome to ASP.NET MVC!";
@@ -43,20 +55,15 @@ namespace KnockoutJS.Controllers
         
         public JsonResult GetPeopleViaAjax()
         {
-            var people = new List<Person>
-                             {
-                                 new Person {Name = "권효중", Address = "Songdo"},
-                                 new Person {Name = "이수진", Address = "Seoul"},
-                                 new Person {Name = "박지성", Address = "London"}
-                             };
-
-            return Json(people, JsonRequestBehavior.AllowGet);
+            return Json(_people, JsonRequestBehavior.AllowGet);
         }
 
         [HttpPost]
         public JsonResult Create(List<Person> people)
         {
-            var message = people.Aggregate("결과값\r\n", 
+            _people = people;
+
+            var message = _people.Aggregate("결과값\r\n", 
                 (current, person) => current + string.Format("{0} {1} {2}", person.Name, person.Address, Environment.NewLine));
             
             return Json(message);
