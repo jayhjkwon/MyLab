67384348f72008f72e8457e687a2665d33fdc8a4
Ray H.J Kwon <nicekhj@gmail.com>
2012-05-11 17:46:48 +0900

    added one page web sample

--- KnockoutJS/Controllers/HomeController.cs
+++ KnockoutJS/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ namespace KnockoutJS.Controllers
     public class HomeController : Controller
     {
         private static List<Person> _people;
+        private List<Patient> _patients; 
  
         public HomeController()
         {
@@ -19,6 +20,14 @@ namespace KnockoutJS.Controllers
                                  new Person {Name = "이수진", Address = "Seoul"},
                                  new Person {Name = "박지성", Address = "London"}
                              };
+
+            _patients = _patients ?? new List<Patient>
+                                         {
+                                             new Patient {Name = "권효중", Address = "송도", Age = "20"},
+                                             new Patient {Name = "한원", Address = "수원", Age = "30"},
+                                             new Patient {Name = "서영태", Address = "서울", Age = "40"},
+                                             new Patient {Name = "마이클", Address = "인천", Age = "50"}
+                                         };
         }
         public ActionResult Index()
         {
@@ -69,11 +78,24 @@ namespace KnockoutJS.Controllers
             return Json(message);
         }
 
+        public JsonResult GetPatientInfo(string patientId)
+        {
+
+            return Json(_patients.FirstOrDefault(p=>p.Name == patientId), JsonRequestBehavior.AllowGet);
+        }
+
     }
 
     public class Person
     {
         public string Name { get; set; }
         public string Address { get; set; }
+    }
+
+    public class Patient
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Age { get; set; }
     }
 }
