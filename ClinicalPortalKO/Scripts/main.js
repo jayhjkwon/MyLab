89d7e42cda53f1e262ec276d4a44651a7541ce57
Ray H.J Kwon <nicekhj@gmail.com>
2012-06-08 17:32:56 +0900

    added namespace utility funciton

---
+++ ClinicalPortalKO/Scripts/main.js
@@ -0,0 +1,35 @@
+﻿
+/***********************************
+* define root namespace
+***********************************/
+
+var ktc = ktc || {};
+
+
+/***********************************
+* define namespace utility function
+*
+* namespace() utility function's usage is as follow
+*
+*   ktc.namespace('ktc.model.user');
+*   ktc.model.user = (function () {
+*       var name = 'khj';
+*       var getFullName = function () { return name; };
+*       return { getFullName: getFullName };
+*   })();
+*
+*   var n = ktc.model.user.getFullName();
+*
+***********************************/
+
+ktc.namespace = function (namespaceString) {
+    var parts = namespaceString.split('.'),
+        parent = window,
+        currentPart = '';
+
+    for (var i = 0, length = parts.length; i < length; i++) {
+        currentPart = parts[i];
+        parent[currentPart] = parent[currentPart] || {};
+        parent = parent[currentPart];
+    }
+}
