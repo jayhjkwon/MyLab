3ddfeaaa9a738806ec92278faaca323c96893e69
Ray H.J Kwon <nicekhj@gmail.com>
2012-07-30 19:12:33 +0900

    create custom jquery plugin for left menu expander

---
+++ CssTest/Scripts/jquery.ereferral.makeLeftPaneExpandable.js
@@ -0,0 +1,144 @@
+﻿(function ($) {
+
+    $.extend($, {
+
+        makeLeftPaneExpandable: function (options) {
+            
+            var defaults;
+
+            defaults = {
+                speed: 300
+                , docListWrapperExpandWidth: 100
+                , docListWrapperCollapseWidth: 30
+                , thumbnailWrapperExpandWidth: 80
+                , thumbnailWrapperCollapseWidth: 30
+                , $docListWrapper: undefined
+                , $thumbnailWrapper: undefined
+                , $docViewerWrapper: undefined
+                , $btnDocListExpander: undefined
+                , $btnThumbnailExpander: undefined
+            };
+
+            // map the options object's value to the defaults object
+            $.extend(defaults, options);
+
+            // check must-have-value parameters
+            if (!defaults.$docListWrapper) {
+                throw new Error('The $docListWrapper in options object must have value');
+                return;
+            }
+            if (!defaults.$thumbnailWrapper) {
+                throw new Error('The $thumbnailWrapper in options object must have value');
+                return;
+            }
+            if (!defaults.$docViewerWrapper) {
+                throw new Error('The $docViewerWrapper in options object must have value');
+                return;
+            }
+            if (!defaults.$btnDocListExpander) {
+                throw new Error('The $btnDocListExpander in options object must have value');
+                return;
+            }
+            if (!defaults.$btnThumbnailExpander) {
+                throw new Error('The $btnThumbnailExpander in options object must have value');
+                return;
+            }
+            
+
+            defaults.$btnDocListExpander.click(function () {
+                performAction(!isExpandedLeftPane(defaults), isExpandedThumbnailPane(defaults), defaults);
+            });
+
+            defaults.$btnThumbnailExpander.click(function () {
+                performAction(isExpandedLeftPane(defaults), !isExpandedThumbnailPane(defaults), defaults);
+            });
+
+            return this;
+        }   // end of the 'makeLeftMenuExpandable' function
+    });
+
+
+    // private functions
+    var performAction = function (expandDoc, expandThumb, defaults) {
+        var windowWidth = $(window).width();
+
+        if (expandDoc) {
+            if (expandThumb) {
+                defaults.$btnDocListExpander.text('<');
+                defaults.$btnThumbnailExpander.text('<');
+
+                defaults.$docListWrapper.animate({ width: defaults.docListWrapperExpandWidth }, defaults.speed);
+                defaults.$thumbnailWrapper.animate({
+                    left: defaults.docListWrapperExpandWidth,
+                    width: defaults.thumbnailWrapperExpandWidth
+                }, defaults.speed);
+                defaults.$docViewerWrapper.animate({
+                    left: defaults.docListWrapperExpandWidth + defaults.thumbnailWrapperExpandWidth,
+                    width: windowWidth - defaults.docListWrapperExpandWidth - defaults.thumbnailWrapperExpandWidth
+                }, defaults.speed);
+            } else {
+                defaults.$btnDocListExpander.text('<');
+                defaults.$btnThumbnailExpander.text('>');
+
+                defaults.$docListWrapper.animate({ width: defaults.docListWrapperExpandWidth }, defaults.speed);
+                defaults.$thumbnailWrapper.animate({
+                    left: defaults.docListWrapperExpandWidth,
+                    width: defaults.thumbnailWrapperCollapseWidth
+                }, defaults.speed);
+                defaults.$docViewerWrapper.animate({
+                    left: defaults.docListWrapperExpandWidth + defaults.thumbnailWrapperCollapseWidth,
+                    width: windowWidth - defaults.docListWrapperExpandWidth - defaults.thumbnailWrapperCollapseWidth
+                }, defaults.speed);
+            }
+        } else {
+            if (expandThumb) {
+                defaults.$btnDocListExpander.text('>');
+                defaults.$btnThumbnailExpander.text('<');
+
+                defaults.$docListWrapper.animate({ width: defaults.docListWrapperCollapseWidth }, defaults.speed);
+                defaults.$thumbnailWrapper.animate({
+                    left: defaults.docListWrapperCollapseWidth,
+                    width: defaults.thumbnailWrapperExpandWidth
+                }, defaults.speed);
+                defaults.$docViewerWrapper.animate({
+                    left: defaults.docListWrapperCollapseWidth + defaults.thumbnailWrapperExpandWidth,
+                    width: windowWidth - defaults.docListWrapperCollapseWidth - defaults.thumbnailWrapperExpandWidth
+                }, defaults.speed);
+            } else {
+                defaults.$btnDocListExpander.text('>');
+                defaults.$btnThumbnailExpander.text('>');
+
+                defaults.$docListWrapper.animate({ width: defaults.docListWrapperCollapseWidth }, defaults.speed);
+                defaults.$thumbnailWrapper.animate({
+                    left: defaults.docListWrapperCollapseWidth,
+                    width: defaults.thumbnailWrapperCollapseWidth
+                }, defaults.speed);
+                defaults.$docViewerWrapper.animate({
+                    left: defaults.docListWrapperCollapseWidth + defaults.thumbnailWrapperCollapseWidth,
+                    width: windowWidth - defaults.docListWrapperCollapseWidth - defaults.thumbnailWrapperCollapseWidth
+                }, defaults.speed);
+            }
+        }
+    };
+
+
+    // return current status of panes, expanded or collapsed
+    var isExpandedLeftPane = function (defaults) {
+        if (defaults.$docListWrapper.width() ===
+                defaults.docListWrapperExpandWidth) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    var isExpandedThumbnailPane = function (defaults) {
+        if (defaults.$thumbnailWrapper.width() ===
+                defaults.thumbnailWrapperExpandWidth) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+})(jQuery);
